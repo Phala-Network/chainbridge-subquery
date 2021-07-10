c2bcd4897645ac51ac2647acbfd20f35a56bf1d8
amphineko <sukareki@gmail.com>
2021-07-10 22:15:13 +0800

    feat: add FungibleTransfer event

--- src/mappings/chainBridgeHandlers.ts
+++ src/mappings/chainBridgeHandlers.ts
@@ -1,7 +1,29 @@
+import { Bytes, U256 } from '@polkadot/types'
 import { IEvent } from '@polkadot/types/types'
 import { SubstrateEvent } from '@subql/types'
-import { ChainId, DepositNonce } from '../interfaces'
-import { ProposalSucceededEvent } from '../types'
+import { ChainId, DepositNonce, ResourceId } from '../interfaces'
+import { FungibleTransferEvent, ProposalSucceededEvent } from '../types'
+
+export async function handleFungibleTransferEvent(ctx: SubstrateEvent): Promise<void> {
+    const {
+        data: [chainIdCodec, depositNonceCodec, resourceId, amount, recipient],
+    } = ctx.event as unknown as IEvent<[ChainId, DepositNonce, ResourceId, U256, Bytes]>
+
+    const chainId = chainIdCodec.toNumber()
+    const depositNonce = depositNonceCodec.toBigInt()
+
+    const id = `${chainId}-${depositNonce}`
+
+    if (undefined === (await FungibleTransferEvent.get(id))) {
+        const record = new FungibleTransferEvent(id)
+        record.amount = amount.toString()
+        record.depositNonce = depositNonce
+        record.destinationChainId = chainId
+        record.recipient = recipient.toHex()
+        record.resourceId = resourceId.toHex()
+        await record.save()
+    }
+}
 
 export async function handleProposalSucceededEvent(ctx: SubstrateEvent): Promise<void> {
     const {
